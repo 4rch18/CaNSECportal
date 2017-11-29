78c0e52e3d8a3a1b3b1fac7150b420e1afe5a925
Archit Agarwal <computer-a@Archits-MacBook-Air.local>
2017-11-29 20:23:20 +0530

    created calculation script, txt files for indicator, subindicator import in /form/other

---
+++ form/__init__.py
@@ -0,0 +1,2 @@
+default_app_config = 'form.apps.FormConfig'
+    # Reference= https://stackoverflow.com/questions/24319558/how-to-resolve-django-core-exceptions-improperlyconfigured-application-labels
